e820d6f43c3f49cf66553c88e243b9997c862655
Anup Sure <sure.anup06@gmail.com>
2026-08-23 19:12:16 +0530

    Shrifted to choice from list and created a base calculate page and home page

--- ShapeAreaCalculator_AWT.java
+++ ShapeAreaCalculator_AWT.java
@@ -3,10 +3,10 @@ import java.awt.event.*;
 
 class ShapeAreaCalculator_AWT
 {
-    Frame f1;
-    Label l1,l2,l3;
-    List list1;
-    Button b1;
+    Frame f1,f2;
+    Label l1,l2,l3,l4;
+    Choice c;
+    Button b1,b2;
 
     ShapeAreaCalculator_AWT()
     {
@@ -17,21 +17,31 @@ class ShapeAreaCalculator_AWT
         l1 = new Label("SHAPE AREA CALCULATER");
         l1.setBounds(70,50,158,20);
         
+        b2 = new Button("Select");
+        b2.setBounds(40,150,60,20);
+        b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+            {
+                String data = "The Shape You Selected is :" + "    " +c.getItem(c.getSelectedIndex());
+                l3.setText(data);
+            }
+        });
+
         l2 = new Label("Select A shape :");
         l2.setBounds(30,100,90,20);
 
-        list1 = new List(4,false);
-        list1.add("Circle");
-        list1.add("Squair");
-        list1.add("Rectangle");
-        list1.add("Triangle");
-        list1.add("Parallelogram");
-        list1.add("Rombus");
-        list1.add("Trapezium");
-        list1.add("Kite");
-        list1.add("Ellipse");
-        list1.add("Hexagon"); 
-        list1.setBounds(140,100,100,60);
+        c = new Choice();
+        c.add("Circle");
+        c.add("Squair");
+        c.add("Rectangle");
+        c.add("Triangle");
+        c.add("Parallelogram");
+        c.add("Rombus");
+        c.add("Trapezium");
+        c.add("Kite");
+        c.add("Ellipse");
+        c.add("Hexagon"); 
+        c.setBounds(140,100,100,40);
 
         l3 = new Label("The Shape You Selected is :");
         l3.setBounds(20,200,265,20);
@@ -39,11 +49,30 @@ class ShapeAreaCalculator_AWT
         b1 = new Button("Calculate");
         b1.setBounds(110,250,60,20);
 
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+            {
+                f2 = new Frame("Calculater");
+                f2.setSize(300,400);
+                f2.setLayout(null);
+
+                l4 = new Label(c.getItem(c.getSelectedIndex()) + " " + "Area calculater");
+                l4.setBounds(70,50,158,20);
+
+
+                f2.add(l4);
+                f1.setVisible(false);
+                f2.setVisible(true);
+ 
+            }
+        });
+
         f1.add(l1);
         f1.add(l2);
-        f1.add(list1);
+        f1.add(c);
         f1.add(l3);
         f1.add(b1);
+        f1.add(b2);
         f1.setVisible(true);
 
     }
